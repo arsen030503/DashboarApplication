458a2b4fe9b7e7d5cdda6215e08c478021c48ddb
arsen <arsen.suranov2003@gmail.com>
2025-04-27 01:35:58 +0600

    fixed errors

--- LiveChartsDashboard/MainWindow.axaml.cs
+++ LiveChartsDashboard/MainWindow.axaml.cs
@@ -2,6 +2,8 @@ using Avalonia.Controls;
 using System.Collections.ObjectModel;
 using LiveChartsCore;
 using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
 
 namespace DashboardApp
 {
@@ -14,18 +16,43 @@ namespace DashboardApp
         {
             InitializeComponent();
 
-            // Пример данных для графиков
+            // Линейный график с полной настройкой
             LineSeries = new ObservableCollection<ISeries>
             {
-                new LineSeries<double> { Values = new double[] { 3, 5, 7, 9, 4 } }
+                new LineSeries<double>
+                {
+                    Values = new double[] { 3, 5, 7, 9, 4 },
+                    Name = "Данные",
+                    Stroke = new SolidColorPaint(SKColors.Blue) { StrokeThickness = 4 },
+                    Fill = null,
+                    GeometrySize = 10,
+                    GeometryStroke = new SolidColorPaint(SKColors.Blue) { StrokeThickness = 2 }
+                }
             };
 
+            // Круговая диаграмма с полной настройкой
             PieSeries = new ObservableCollection<ISeries>
             {
-                new PieSeries<double> { Values = new double[] { 40 } },
-                new PieSeries<double> { Values = new double[] { 30 } },
-                new PieSeries<double> { Values = new double[] { 20 } },
-                new PieSeries<double> { Values = new double[] { 10 } }
+                new PieSeries<double> { 
+                    Values = new double[] { 40 },
+                    Name = "Категория 1",
+                    Fill = new SolidColorPaint(SKColors.Red)
+                },
+                new PieSeries<double> { 
+                    Values = new double[] { 30 },
+                    Name = "Категория 2",
+                    Fill = new SolidColorPaint(SKColors.Green)
+                },
+                new PieSeries<double> { 
+                    Values = new double[] { 20 },
+                    Name = "Категория 3",
+                    Fill = new SolidColorPaint(SKColors.Blue)
+                },
+                new PieSeries<double> { 
+                    Values = new double[] { 10 },
+                    Name = "Категория 4",
+                    Fill = new SolidColorPaint(SKColors.Yellow)
+                }
             };
 
             DataContext = this;
--- LiveChartsDashboard/obj/Debug/net9.0/LiveChartsDashboard.AssemblyInfo.cs
+++ LiveChartsDashboard/obj/Debug/net9.0/LiveChartsDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiveChartsDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+953fc44f8dbfdcb0d898e18cba20d3c4c26b396b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LiveChartsDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LiveChartsDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
